bb188f5399af0e080330e1806622f1b716d98315
flora0110 <58814336+flora0110@users.noreply.github.com>
2021-07-23 05:51:12 +0800

    page

---
+++ network_graph.js
@@ -0,0 +1,36 @@
+var color=[]
+for(i=0;i<145;i++){
+    color[i]='#4caf50';
+}
+
+const Graph = ForceGraph3D()
+  (document.getElementById('3d-graph'))
+    .jsonUrl('https://raw.githubusercontent.com/flora0110/network_graph/master/node_and_link.json')
+    .nodeColor('color')
+    .nodeThreeObject(node => {
+      const sprite = new SpriteText(node.id);
+      sprite.material.depthWrite = false; // make sprite background transparent
+      console.log(node.color)
+      sprite.color =node.color;
+      sprite.textHeight = node.group;
+      return sprite;
+    })
+    .linkThreeObjectExtend(true)
+    .linkThreeObject(link => {
+      // extend link with text sprite
+      const sprite = new SpriteText(`${link.source} > ${link.target}`);
+      sprite.color = 'white';
+      sprite.textHeight = 1.5;
+      return sprite;
+    })
+    .linkPositionUpdate((sprite, { start, end }) => {
+      const middlePos = Object.assign(...['x', 'y', 'z'].map(c => ({
+        [c]: start[c] + (end[c] - start[c]) / 2 // calc middle point
+      })));
+      // Position sprite
+      Object.assign(sprite.position, middlePos);
+    })
+    .linkWidth ('value');
+
+// Spread nodes a little wider
+Graph.d3Force('charge').strength(-120);
